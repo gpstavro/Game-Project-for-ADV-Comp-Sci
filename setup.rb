e894be5c843d3431b649e6f311b603f03e030a39
gpstavro <gstavropoulos@browning.edu>
2017-12-04 08:29:15 -0500

    Update setup.rb

--- setup.rb
+++ setup.rb
@@ -1,3 +1,4 @@
+require_relative "./Scenes.rb"
 
 class Engine
 
@@ -7,14 +8,46 @@ class Engine
 
   def play()
     current_scene = @scene_map.opening_scene()
-    last_scene = @scene_map.next_scene('finished')
+    last_scene = @scene_map.next_scene('end II')
 
     while current_scene != last_scene
       next_scene_name = current_scene.enter()
       current_scene = @scene_map.next_scene(next_scene_name)
     end
 
-    # be sure to print out the last scene
     current_scene.enter()
   end
 end
+
+class Map
+  @@scenes = {
+    'intro' => Intro.new(),
+    # 'menu' => Menu,
+    'name' => Name.new(),
+    'breakfast' => Breakfast.new(),
+    'end I' => END_I,
+    'end II' => END_II,
+    'home' => Home,
+    'hiking' => Hike,
+    'practice' =>Practice,
+    'quit' => Quit.new(),
+  }
+  
+  def initialize(start)
+    @start = start
+  end
+
+
+  def next_scene(scene_name)
+    val = @@scenes[scene_name]
+    return val
+  end
+
+  def opening_scene()
+    return next_scene(@start)
+  end
+end
+
+a_map = Map.new('intro')
+a_game = Engine.new(a_map)
+a_game.play()
